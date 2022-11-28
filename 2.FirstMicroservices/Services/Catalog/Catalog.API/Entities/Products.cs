6e1821186cf6d23df8ba3120ba1ee4c3d983cb44
nguyen.huynh-thao <nguyen.huynh-thao@banvien.com.vn>
2022-11-28 11:26:52 +0700

    Section2: Developing first microservices

---
+++ 2.FirstMicroservices/Services/Catalog/Catalog.API/Entities/Products.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = default!;
+
+        [BsonElement("Name")]
+        public string Name { get; set; } = default!;
+
+        public string Category { get; set; } = default!;
+        public string Summary { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+    }
+}
